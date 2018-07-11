d6a0b4c735ce4ca633d82e9847960e34303ee6cc
Hangjit Rai <hangjit.rai@adslot.com>
2018-07-12 09:16:40 +1000

    Added chocolatey scripts

---
+++ package_list1.bat
@@ -0,0 +1,21 @@
+:: choco install dotnet3.5 -y >> %COMPUTERNAME%.log
+:: choco install hipchat -y >> %COMPUTERNAME%.log
+:: choco install nunit -version 2.6.4 -y >> %COMPUTERNAME%.log
+:: choco install skype -y >> %COMPUTERNAME%.log
+choco install 7zip -y >> %COMPUTERNAME%.log
+choco install ccleaner -y >> %COMPUTERNAME%.log
+choco install fiddler4 -y >> %COMPUTERNAME%.log
+choco install firefox -y >> %COMPUTERNAME%.log
+choco install firefox -y >> %COMPUTERNAME%.log
+choco install foxitreader -y >> %COMPUTERNAME%.log
+choco install git -y >> %COMPUTERNAME%.log
+choco install google-chrome-x64 -y >> %COMPUTERNAME%.log
+choco install googledrive -y >> %COMPUTERNAME%.log
+choco install nodejs -y >> %COMPUTERNAME%.log
+choco install notepadplusplus -y >> %COMPUTERNAME%.log
+choco install powershell4 -y >> %COMPUTERNAME%.log
+choco install resharper -version 9.2.0.0 -y >> %COMPUTERNAME%.log
+choco install visualstudio2017professional -y >> %COMPUTERNAME%.log
+choco install vscode -y >> %COMPUTERNAME%.log
+:: @echo Now restart to finalise the .Net 3.5 install
+
