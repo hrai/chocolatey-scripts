011c7d0fff63857fc378ef19dcefdb8d85d6781d
Hangjit Rai <hangjit.rai@adslot.com>
2018-07-12 09:29:53 +1000

    Added more programs

--- package_list1.bat
+++ package_list1.bat
@@ -4,17 +4,26 @@
 :: choco install skype -y >> %COMPUTERNAME%.log
 choco install 7zip -y >> %COMPUTERNAME%.log
 choco install ccleaner -y >> %COMPUTERNAME%.log
+choco install dotnetcore -y >> %COMPUTERNAME%.log
+choco install dotnetcore-sdk  -y >> %COMPUTERNAME%.log
 choco install fiddler4 -y >> %COMPUTERNAME%.log
 choco install firefox -y >> %COMPUTERNAME%.log
 choco install firefox -y >> %COMPUTERNAME%.log
 choco install foxitreader -y >> %COMPUTERNAME%.log
 choco install git -y >> %COMPUTERNAME%.log
+choco install git-credential-manager-for-windows -y >> %COMPUTERNAME%.log
+choco install gitextensions -y >> %COMPUTERNAME%.log
 choco install google-chrome-x64 -y >> %COMPUTERNAME%.log
+choco install googledrive -y >> %COMPUTERNAME%.log
 choco install googledrive -y >> %COMPUTERNAME%.log
 choco install nodejs -y >> %COMPUTERNAME%.log
 choco install notepadplusplus -y >> %COMPUTERNAME%.log
+choco install postman -y >> %COMPUTERNAME%.log
 choco install powershell4 -y >> %COMPUTERNAME%.log
 choco install resharper -version 9.2.0.0 -y >> %COMPUTERNAME%.log
+choco install spotify -y >> %COMPUTERNAME%.log
+choco install sql-server-management-studio -y >> %COMPUTERNAME%.log
+choco install vim -y >> %COMPUTERNAME%.log
 choco install visualstudio2017professional -y >> %COMPUTERNAME%.log
 choco install vscode -y >> %COMPUTERNAME%.log
 :: @echo Now restart to finalise the .Net 3.5 install
