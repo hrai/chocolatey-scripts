c7a2bc04b4fcbe6a6460aea94323ea5887ada447
hrai <rai.hangjit@gmail.com>
2018-08-14 10:09:02 +1000

    Added packages

--- package_list1.bat
+++ package_list1.bat
@@ -1,26 +1,28 @@
-:: choco install dotnet3.5 -y >> %COMPUTERNAME%.log
-:: choco install hipchat -y >> %COMPUTERNAME%.log
-:: choco install nunit -version 2.6.4 -y >> %COMPUTERNAME%.log
-:: choco install skype -y >> %COMPUTERNAME%.log
+:: Utilities
 choco install 7zip -y >> %COMPUTERNAME%.log
 choco install ccleaner -y >> %COMPUTERNAME%.log
-choco install dotnetcore -y >> %COMPUTERNAME%.log
-choco install dotnetcore-sdk  -y >> %COMPUTERNAME%.log
-choco install fiddler4 -y >> %COMPUTERNAME%.log
 choco install firefox -y >> %COMPUTERNAME%.log
 choco install foxitreader -y >> %COMPUTERNAME%.log
+choco install google-chrome-x64 -y >> %COMPUTERNAME%.log
+choco install googledrive -y >> %COMPUTERNAME%.log
+choco install nodejs -y >> %COMPUTERNAME%.log
+choco install notepadplusplus -y >> %COMPUTERNAME%.log
+choco install spotify -y >> %COMPUTERNAME%.log
+
+:: Dev tools
+choco install dotnetcore -y >> %COMPUTERNAME%.log
+choco install dotnetcore-sdk  -y >> %COMPUTERNAME%.log
+choco install docker -y >> %COMPUTERNAME%.log
+choco install fiddler4 -y >> %COMPUTERNAME%.log
 choco install git -y >> %COMPUTERNAME%.log
 choco install git-credential-manager-for-windows -y >> %COMPUTERNAME%.log
 choco install gitextensions -y >> %COMPUTERNAME%.log
-choco install google-chrome-x64 -y >> %COMPUTERNAME%.log
-choco install googledrive -y >> %COMPUTERNAME%.log
 choco install kdiff3 -y >> %COMPUTERNAME%.log
-choco install nodejs -y >> %COMPUTERNAME%.log
-choco install notepadplusplus -y >> %COMPUTERNAME%.log
 choco install postman -y >> %COMPUTERNAME%.log
+choco install postsharp -y >> %COMPUTERNAME%.log
 choco install powershell4 -y >> %COMPUTERNAME%.log
+choco install rabbitmq -y >> %COMPUTERNAME%.log
 choco install resharper -y >> %COMPUTERNAME%.log
-choco install spotify -y >> %COMPUTERNAME%.log
 choco install sql-server-management-studio -y >> %COMPUTERNAME%.log
 choco install vim -y >> %COMPUTERNAME%.log
 choco install visualstudio2017professional -y >> %COMPUTERNAME%.log
