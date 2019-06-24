6d579206bd8ec28a08fdaa68153ee742b017a932
Hangjit Rai <Hangjit.Rai@dg.domain.com.au>
2019-06-24 16:59:28 +1000

    removed python3

--- choco_install.bat
+++ choco_install.bat
@@ -26,13 +26,13 @@ choco install kdiff3 -y >> %COMPUTERNAME%.log
 choco install postman -y >> %COMPUTERNAME%.log
 choco install postsharp -y >> %COMPUTERNAME%.log
 choco install powershell5 -y >> %COMPUTERNAME%.log
-choco install python --version 3.6.3 --x86force -y >> %COMPUTERNAME%.log
+::choco install python --version 3.6.3 --x86force -y >> %COMPUTERNAME%.log
 choco install pip --x86force >> %COMPUTERNAME%.log
 choco install rabbitmq -y >> %COMPUTERNAME%.log
 choco install resharper -y >> %COMPUTERNAME%.log
 choco install sql-server-management-studio -y >> %COMPUTERNAME%.log
 choco install vim -y >> %COMPUTERNAME%.log
-choco install visualstudio2017professional -y >> %COMPUTERNAME%.log
+choco install visualstudio2019professional -y >> %COMPUTERNAME%.log
 choco install vscode -y >> %COMPUTERNAME%.log
 
 :: @echo Now restart to finalise the .Net 3.5 install
