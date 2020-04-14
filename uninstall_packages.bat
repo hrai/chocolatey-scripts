08c463688d950d883462761a52303c59dc052fc8
Hangjit Rai <rai.hangjit@gmail.com>
2020-04-14 17:15:36 +1000

    Cleanup

--- uninstall_packages.bat
+++ uninstall_packages.bat
@@ -1,9 +1,9 @@
 choco uninstall spotify -y >> %COMPUTERNAME%.log
+choco uninstall erlang -y >> %COMPUTERNAME%.log
 choco uninstall conemu -y >> %COMPUTERNAME%.log
 choco uninstall dotnetcore -y >> %COMPUTERNAME%.log
 choco uninstall docker-desktop -y >> %COMPUTERNAME%.log
 choco uninstall powershell5 -y >> %COMPUTERNAME%.log
-choco uninstall python3 -y >> %COMPUTERNAME%.log
 choco uninstall rabbitmq -y >> %COMPUTERNAME%.log
 choco uninstall resharper -y >> %COMPUTERNAME%.log
 choco uninstall sql-server-management-studio -y >> %COMPUTERNAME%.log
