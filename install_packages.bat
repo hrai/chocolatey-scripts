dd5bcdec113652c333d0017257aa54f5711af0e5
Hangjit Rai <hangjit.rai@outlook.com>
2020-04-15 09:37:35 +1000

    Added Nswag

--- install_packages.bat
+++ install_packages.bat
@@ -23,6 +23,7 @@ choco upgrade git -y >> %COMPUTERNAME%.log
 choco upgrade git-credential-manager-for-windows -y >> %COMPUTERNAME%.log
 choco upgrade gitextensions -y >> %COMPUTERNAME%.log
 choco upgrade kdiff3 -y >> %COMPUTERNAME%.log
+choco upgrade nswagstudio -y >> %COMPUTERNAME%.log
 choco upgrade postman -y >> %COMPUTERNAME%.log
 :: choco upgrade postsharp -y >> %COMPUTERNAME%.log
 :: choco upgrade powershell5 -y >> %COMPUTERNAME%.log
