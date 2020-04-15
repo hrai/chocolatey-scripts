f3948c5550bd4646a3cfe74e5d9c9639594a8987
Hangjit Rai <hangjit.rai@outlook.com>
2020-04-15 10:59:48 +1000

    Disabling Vs Pro

--- install_packages.bat
+++ install_packages.bat
@@ -22,17 +22,13 @@ choco upgrade gitextensions -y >> %COMPUTERNAME%.log
 choco upgrade kdiff3 -y >> %COMPUTERNAME%.log
 choco upgrade nswagstudio -y >> %COMPUTERNAME%.log
 choco upgrade postman -y >> %COMPUTERNAME%.log
-:: choco upgrade postsharp -y >> %COMPUTERNAME%.log
-:: choco upgrade powershell5 -y >> %COMPUTERNAME%.log
-::choco upgrade python --version 3.6.3 --x86force -y >> %COMPUTERNAME%.log
 choco upgrade python3 -y >> %COMPUTERNAME%.log
-::choco upgrade pip --x86force >> %COMPUTERNAME%.log
 choco upgrade pip3 -y >> %COMPUTERNAME%.log
 :: choco upgrade rabbitmq -y >> %COMPUTERNAME%.log
 :: choco upgrade resharper -y >> %COMPUTERNAME%.log
 choco upgrade universal-ctags -y >> %COMPUTERNAME%.log
 choco upgrade vim-tux.install /InstallPopUp -y >> %COMPUTERNAME%.log
-choco upgrade visualstudio2019professional -y >> %COMPUTERNAME%.log
+:: choco upgrade visualstudio2019professional -y >> %COMPUTERNAME%.log
 choco upgrade vscode -y >> %COMPUTERNAME%.log
 choco upgrade microsoft-windows-terminal -y >> %COMPUTERNAME%.log
 choco upgrade yarn -y >> %COMPUTERNAME%.log
