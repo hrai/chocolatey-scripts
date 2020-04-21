5afa9b739b544a85ee83549093b7598e244fc4b6
Hangjit Rai <rai.hangjit@gmail.com>
2020-04-21 16:12:45 +1000

    Added Git Shortcuts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,31 @@ function ... { set-location ... }
 function x {
     Invoke-command -ScriptBlock {exit}
 }
+
+function v {
+    nvim $args
+}
+
+function vim {
+    v $args
+}
+
+function gs {
+	git status $args
+}
+
+function Get-Git-CurrentBranch {
+	git symbolic-ref --quiet HEAD *> $null
+
+	if ($LASTEXITCODE -eq 0) {
+		return git rev-parse --abbrev-ref HEAD
+	} else {
+		return
+	}
+}
+function gap {
+    $branch_name = Get-Git-CurrentBranch
+	git add .;
+	git commit -m "$args";
+	git push -u origin $branch_name;
+}
